965acd9624b9f0121f1ebac97530fe8accb970d3
Crowdbotics <team@crowdbotics.com>
2021-04-03 23:52:16 +0000

    Update models

--- backend/shot_timer_25450/wsgi.py
+++ backend/shot_timer_25450/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shot_timer_25450.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shot_timer_25450.settings")
 
 application = get_wsgi_application()
