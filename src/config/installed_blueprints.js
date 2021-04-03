0fff4e3988460ebb2a1f3434c8420f3f653d30be
Crowdbotics <team@crowdbotics.com>
2021-04-03 23:53:11 +0000

    add package blueprint-react-native-message-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Messaging221774', human_name: 'Messaging', access_route: 'Messaging221774', icon: 'comments'},
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ]
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from "@react-navigation/native"
 import SplashScreen from "../features/SplashScreen"
 import SideMenu from "./sideMenu"
 //@BlueprintImportInsertion
+import Messaging221774Navigator from '../features/Messaging221774/navigator';
 
 /**
  * new navigators can be imported here
@@ -12,6 +13,7 @@ import SideMenu from "./sideMenu"
 
 const AppNavigator = {
   //@BlueprintNavigationInsertion
+Messaging221774: { screen: Messaging221774Navigator },
 
   /** new navigators can be added here */
   SplashScreen: {
